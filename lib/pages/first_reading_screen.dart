1672916399229b9627c8d84cfdc00c68efce6ea4
Romuald H <romulus17fr@yahoo.fr>
2023-01-18 17:03:15 +0100

    Setting first page

--- lib/pages/first_reading_screen.dart
+++ lib/pages/first_reading_screen.dart
@@ -10,6 +10,34 @@ class FirstReading extends StatefulWidget {
 class _FirstReadingState extends State<FirstReading> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Center(
+      child:  Container(
+        padding: EdgeInsets.all(10.0),
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget> [
+            Expanded(
+              child: Text("E de sin wema  ",
+              style: TextStyle(decoration: TextDecoration.none,
+              fontSize: 35.0,
+              fontWeight: FontWeight.bold),),
+            ),
+            Expanded(child: Text("bena  nfwofno  lòwjfojoij jnfpiwqhfpojqq jbnipwqshdfpoihfpihfpiqfhjpowjhfpoisjfpoifjpiowqfjhiopjhfisfhjpoisvhjspqiofjhischjvliòsCHJsliòcjhilsDHJòodjhnn   jbiohflihaFLI  ",
+              style: TextStyle(decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),)
+            ),
+            Expanded(
+              child: Text("AQ NFGEWFJEOPP NNRWPOGEW nkjfewofgò oljfoegfjr3oègjjr4gj vborghowghoewr ewnfoqgqehjfg gnwegoiahjegòwelgj jnewogjhsdpogaeusfae jgreogwrgjupwrgwero jnrfogjhrwgporwgjw brwngwrglijrwhg rgjrprwogijrw fsgowhjgqwepogqlfnmnwpr noghreogrweghijrw grhgrwlghjogirejgr hreogihrwlighwerpgowrhjg jgwroigjpreoijgroi jgreowghjpoiwlejgpoiergiohjgpiglerjgoipjrwgoihrwoihgoiwvhkwfnwijvbserkghvewlrighewilgjloi34,kgu3j ejnghiulghr4ij4r ger gherighjekigjeoi ",
+                style: TextStyle(decoration: TextDecoration.none,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),),
+            )
+          ],
+          
+        ),
+      ),
+
+    );
   }
 }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wema/pages/first_reading_screen.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -19,7 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.redAccent,
         iconTheme: const IconThemeData(
             color: Colors.white,
         ) ,
@@ -54,15 +55,10 @@ class _HomeScreenState extends State<HomeScreen> {
           body: const TabBarView(
 
             children: <Widget>[
+              FirstReading(),
+              Text("Psaume "),
+              Text("Evangile     ....")
 
-              Center(
-                child: Text("Nuxixa nukɔntɔn ɔ́. Première lecture. È ɖe sín wemá è è wlán sɛ́dó Eblée lɛ́ɛ é mɛ:   He. 5, 1-10 Vɔ̌sánú-xwlémawutɔ́ ɖaxó ɖěbǔ ɔ́, gbɛtɔ́ lɛ́ɛ mɛ wɛ è nɔ sɔ́ ɖe; è ɖe e nú é ná wa azɔ̌ nú Mawu nú ɖagbe yětɔn. É nɔ sɔ́ nǔníná lɛ́ɛ kpó vɔ̌sánú lɛ́ɛ kpó xwlé Mawu ɖó hwɛ e gbɛtɔ́ lɛ́ɛ hu lɛ́ɛ tamɛ. Ée é ɖésúnɔ lɔ nyí mɛ, bó ma ɖó gǎn ǎ gbɔn alɔkpa gěgé mɛ é ɔ́, é sixú kpɔ́n bó ɖɔ xá mɛ e ma tunwun nǔɖé ǎ lɛ́ɛ")),
-              Center(
-                child: Text("Ɖɛhan ɔ́. (Psaume responsorial). Ps. 109 (110), 1. 2. 3. 4 R/ Vɔsánú-xwlémawutɔ́ wɛ a nyǐ káká    Cf. Ps. 109, 4 sɔ́yi mavɔ mavɔ, Mɛlukisedɛ́ki ɖɔhun."),
-              ),
-              Center(
-                child: Text("WƐNƉAGBE Ɔ́. (Evangile). Wɛnɖagbe Jezu Klisu tɔn è yɛhwe Maki dó mǐ ɔ́ sín akpáxwé ɖokpó ɖíè: Mk. 2, 18-22"),
-              ),
             ],
           ),
         )
